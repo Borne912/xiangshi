9dcb5a08fba0740ae8a359c16fd113f225ae2d9b
zfx <1321772317@qq.com>
2020-07-24 23:06:32 +0800

    7.24

--- xiangshi/miniprogram/pages/myAddress/myAddress.js
+++ xiangshi/miniprogram/pages/myAddress/myAddress.js
@@ -122,7 +122,9 @@ Page({
   /* 读取地址数据 */
   getData: function(e) {
     var that = this
-    db.collection("myAddress").get({
+    db.collection("myAddress").where({
+       _openid: app.globalData.openid
+    }).get({
       success(res) {
         var rd = res.data
         for(var i = 0; i< rd.length; i++) {
@@ -202,7 +204,7 @@ Page({
       }
     }
     db.collection("myAddress").where({
-      _openid: "ofl4t5MZMq5wGYXzNUYbJ_Uer8DY",
+      _openid: app.globalData.openid,
       default: true
     }).update({
       data: {
@@ -210,7 +212,7 @@ Page({
       },success(res) {
         console.log('全置false成功')
         db.collection("myAddress").where({
-          _openid: "ofl4t5MZMq5wGYXzNUYbJ_Uer8DY",
+          _openid: app.globalData.openid,
           _id: id
         }).update({
           data: {
